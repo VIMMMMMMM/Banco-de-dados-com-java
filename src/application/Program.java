808d63f55ee69640ec504a0cfb769b8372f65816
Eduardo Rocha dos Santos <du.rocha.santos@gmail.com>
2024-01-30 05:47:59 -0300

    inicializando classe seller na classe program

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,9 @@ package application;
 
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -9,5 +12,7 @@ public class Program {
     public static void main(String[] args) {
         Department obj = new Department(1,"livros");
         System.out.println(obj);
+        Seller seller = new Seller(21,"bob","bob@gmail.com",new Date(),3000.00,obj);
+        System.out.println(seller);
     }
 }
