a5ff6580e29a527bedef9c9018bcaa3efc92fc40
Eduardo Rocha dos Santos <du.rocha.santos@gmail.com>
2024-01-30 05:33:35 -0300

    projeto banco de dados com java

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+
+import model.entities.Department;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Program {
+    public static void main(String[] args) {
+        Department obj = new Department(1,"livros");
+        System.out.println(obj);
+    }
+}
